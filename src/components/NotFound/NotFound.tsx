4a15127a28088d3e0e1459178e758deedcd516c7
md-mobarak <md-mobarak>
2023-09-03 12:08:55 +0600

    404 Not found added img

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
+  const navigate = useNavigate();
+  setTimeout(() => {
+    navigate("/");
+  }, 3000);
   return (
     <div>
-      <h1 className="text-5xl lg:font-bold text-center lg:mt-20 ">
-        404 Not Found
-      </h1>
+      <img
+        src="https://media.licdn.com/dms/image/C5112AQEw1fXuabCTyQ/article-inline_image-shrink_1500_2232/0/1581099611064?e=1697673600&v=beta&t=ysuifpFxKxh8N-1O85Om_P_JvcJhG6AXTIlR-xlcYLY"
+        alt=""
+        width="100%"
+        height="100%"
+      />
     </div>
   );
 };
